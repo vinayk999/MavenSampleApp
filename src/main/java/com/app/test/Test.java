168ea76693f7e0cd545d1b8edbe1573c6ef47550
user <user@192.168.0.5>
2018-09-10 13:41:08 +0530

    1001

--- src/main/java/com/app/test/Test.java
+++ src/main/java/com/app/test/Test.java
@@ -1,11 +1,18 @@
 package com.app.test;
 
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
 public class Test {
 
-	public static void main(String[] args) {
-		
+	@RequestMapping("/show")
+	public  String m1(String[] args) {
+		Logger log=Logger.getLogger(Test.class);
 		int i=1;
-		System.out.println(i);
-		
+		log.debug("debug");
+		log.error("error");
+
+		return "Home";
 	}
 }
